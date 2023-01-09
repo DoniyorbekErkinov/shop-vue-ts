8c7b6b45ffc847148ab03c9cb918f8a8c4a8bfd2
doniyor <erkinovdoniyor03@gmail.com>
2023-01-09 10:29:29 +0500

    initial-commit

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,17 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+// Tailwindcss
+import './index.css'
+// Tailwindcss
+
+// icons package
+import mdiVue from 'mdi-vue/v3'
+import * as mdijs from '@mdi/js'
+// icons package
+const app = createApp(App)
+app.use(store).use(router)
+app.use(mdiVue, {
+    icons: mdijs
+  })
+app.mount('#app')
