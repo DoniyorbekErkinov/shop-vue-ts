e2a4a596efdf117c4da289c599845d03ac2179aa
doniyor <erkinovdoniyor03@gmail.com>
2023-12-04 15:41:49 +0500

    api test

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,28 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import Details from '../views/Datails/Details.vue'
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import Details from "../views/Datails/Details.vue";
+import ApiTest from "../views/api-test/index.vue";
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },  
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
   {
-    path: '/details/:id',
-    name: 'details',
-    component: Details
-  }
-]
+    path: "/details/:id",
+    name: "details",
+    component: Details,
+  },
+  {
+    path: "/api-test",
+    name: "Api Test",
+    component: ApiTest,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
